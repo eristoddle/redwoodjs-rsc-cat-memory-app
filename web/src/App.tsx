200b83dbf58725df30a396a16a98b9f09ca8a7a4
Stephan Miller <53488377+stmiller-shamrock@users.noreply.github.com>
2024-04-20 22:18:37 -0500

    Initial commit

---
+++ web/src/App.tsx
@@ -0,0 +1,24 @@
+import type { ReactNode } from 'react'
+
+import { FatalErrorBoundary, RedwoodProvider } from '@redwoodjs/web'
+import { RedwoodApolloProvider } from '@redwoodjs/web/apollo'
+
+import FatalErrorPage from 'src/pages/FatalErrorPage'
+import Routes from 'src/Routes'
+
+import './index.css'
+interface AppProps {
+  children?: ReactNode
+}
+
+const App = ({ children }: AppProps) => (
+  <FatalErrorBoundary page={FatalErrorPage}>
+    <RedwoodProvider titleTemplate="%PageTitle | %AppTitle">
+      <RedwoodApolloProvider>
+        {children ? children : <Routes />}
+      </RedwoodApolloProvider>
+    </RedwoodProvider>
+  </FatalErrorBoundary>
+)
+
+export default App
